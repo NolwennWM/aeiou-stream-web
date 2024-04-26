2c89aa3eecedfd46215417e20f6181762476feb0
Nolwenn <you@example.com>
2024-04-26 16:14:31 +0200

    adaptation à electron

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,12 +3,20 @@
 import { LayoutMenu } from "./LayoutMenu.js";
 import { VideoHandler } from "./VideoHandler.js";
 
-window.addEventListener("swready", startApp);
-// Service worker for hide 404 message in the console.
-navigator.serviceWorker.register("SWno404.js").then(_=> navigator.serviceWorker.controller
-    ? window.dispatchEvent(new CustomEvent("swready")) // normal reload
-    : navigator.serviceWorker.ready.then(_=> location.reload()) // first load or Ctrl+F5
-);
+if(window.electron)
+{
+    startApp();
+}
+else
+{
+    window.addEventListener("swready", startApp);
+    // Service worker for hide 404 message in the console.
+    navigator.serviceWorker.register("SWno404.js").then(_=> navigator.serviceWorker.controller
+        ? window.dispatchEvent(new CustomEvent("swready")) // normal reload
+        : navigator.serviceWorker.ready.then(_=> location.reload()) // first load or Ctrl+F5
+    );
+}
+
 
 /**
  * Launch all the logic of the application.
